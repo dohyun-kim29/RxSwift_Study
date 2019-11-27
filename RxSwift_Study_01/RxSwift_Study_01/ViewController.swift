4a5941cf711ec1d978bd10335a5cb66026c2b9fe
DohyunKIm <rlaehgus1229@gmail.com>
2019-11-27 18:44:57 +0900

    Study01

--- RxSwift_Study_01/RxSwift_Study_01/ViewController.swift
+++ RxSwift_Study_01/RxSwift_Study_01/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         bindUI()
         // Do any additional setup after loading the view.
     }
-
+    
     func loginButtonTextChanger() {
         if loginButton.isEnabled {
             loginButton.titleLabel?.text = "Not Vaild"
@@ -36,40 +36,44 @@ class ViewController: UIViewController {
     func bindUI() {
         
         (idField.rx.text.orEmpty.map(checkEmailValid1(_:)),
-            idField.rx.text.orEmpty.map(checkEmailVaild2(_:)),
+         idField.rx.text.orEmpty.map(checkEmailVaild2(_:))
             .subscribe(onNext: {b in self.idVaildView.isHidden = b})
-            .disposed(by: disposeBag),
-    
-        
-        (pwField.rx.text.orEmpty.map(checkPasswordValid1(_:)),
-            pwField.rx.text.orEmpty.map(checkPasswordVaild2(_:)),
-            pwField.rx.text.orEmpty.map(checkPasswordVaild3(_:)),
+            .disposed(by: disposeBag))
+         
+         
+         (pwField.rx.text.orEmpty.map(checkPasswordValid1(_:)),
+          pwField.rx.text.orEmpty.map(checkPasswordVaild2(_:)),
+          pwField.rx.text.orEmpty.map(checkPasswordVaild3(_:))
             .subscribe(onNext: {b in self.pwVaildView.isHidden = b})
-            .disposed(by: disposeBag),
-           
-        
-        Observable.combineLatest(
+            .disposed(by: disposeBag))
+          
+          
+          
+          Observable.merge(
             idField.rx.text.orEmpty.map(checkEmailValid1),
-            idField.rx.text.orEmpty.map(checkPasswordVaild2),
+            idField.rx.text.orEmpty.map(checkEmailVaild2),
             pwField.rx.text.orEmpty.map(checkPasswordValid1),
             pwField.rx.text.orEmpty.map(checkPasswordVaild2),
             pwField.rx.text.orEmpty.map(checkPasswordVaild3),
             resultSelector: {s1, s2, s3, s4, s5  in s1 && s2}
-        )
-        
+          )
+            
             .subscribe(onNext: {b in self.loginButton.isEnabled = b})
-        .disposed(by: disposeBag)
-        )
+            .disposed(by: disposeBag)
+            
+            
+            
+            
         
-    )}
+    }
     
     func checkEmailValid1(_ email: String) -> Bool {
         return email.contains("@")
     }
-
+    
     func checkEmailVaild2(_ email: String) -> Bool {
         return email.contains(".com") ||
-        email.contains(".net")
+            email.contains(".net")
     }
     
     func checkPasswordValid1(_ password: String) -> Bool {
@@ -88,14 +92,14 @@ class ViewController: UIViewController {
     
     func checkPasswordVaild3(_ password: String) -> Bool {
         return password.contains("1") ||
-        password.contains("2") ||
-        password.contains("3") ||
-        password.contains("4") ||
-        password.contains("5") ||
-        password.contains("6") ||
-        password.contains("7") ||
-        password.contains("8") ||
-        password.contains("9")
+            password.contains("2") ||
+            password.contains("3") ||
+            password.contains("4") ||
+            password.contains("5") ||
+            password.contains("6") ||
+            password.contains("7") ||
+            password.contains("8") ||
+            password.contains("9")
     }
     
 }
