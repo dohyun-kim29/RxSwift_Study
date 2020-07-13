88820723cd1e82b7620e7abd1403ce4cd17c6000
DohyunKIm <rlaehgus1229@gmail.com>
2020-07-13 19:17:38 +0900

    [Grammar_Study] force progress

--- Grammar_Study/Grammar_Study/Study/Hello.swift
+++ Grammar_Study/Grammar_Study/Study/Hello.swift
@@ -1,19 +1,18 @@
 //
-//  main.swift
+//  Hello.swift
 //  Grammar_Study
 //
 //  Created by DohyunKim on 2020/07/13.
 //  Copyright © 2020 DohyunKim. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import RxSwift
+import RxCocoa
 
 let disposeBag = DisposeBag()
 
 Observable.just("Hello, RxSwift")
-    .subscribe { print($0) }
+    .subscribe{ print($0) }
     .disposed(by: disposeBag)
-
-
 
---
+++ Grammar_Study/Grammar_Study/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grammar_Study
+//
+//  Created by DohyunKim on 2020/07/13.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
