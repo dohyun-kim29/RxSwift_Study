bbad246afa56d268c1fee60c7c44fd408ed7838a
DohyunKIm <rlaehgus1229@gmail.com>
2020-07-24 08:59:08 +0900

    [Rx_Memo] MemoDetailViewController UI

--- Rx_Memo/Rx_Memo/ViewController/MemoDetailViewController.swift
+++ Rx_Memo/Rx_Memo/ViewController/MemoDetailViewController.swift
@@ -11,6 +11,14 @@ import UIKit
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
    
     
+    @IBOutlet weak var listTableView: UITableView!
+    
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
     
     var viewModel: MemoDetailViewModel!
 
