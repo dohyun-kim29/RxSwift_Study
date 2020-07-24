382fc7e3cc34224a4691cea8a4756ca25a585bfe
DohyunKIm <rlaehgus1229@gmail.com>
2020-07-24 15:03:50 +0900

    [Rx_Memo] edit MemoDetailView

--- Rx_Memo/Rx_Memo/ViewController/MemoListViewController.swift
+++ Rx_Memo/Rx_Memo/ViewController/MemoListViewController.swift
@@ -14,14 +14,14 @@ import NSObject_Rx
 class MemoListViewController: UIViewController, ViewModelBindableType {
     
     var viewModel: MemoListViewModel!
-
+    
     @IBOutlet weak var listTableView: UITableView!
     
     @IBOutlet weak var addButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -39,16 +39,24 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
         .disposed(by: rx.disposeBag)
         
         addButton.rx.action = viewModel.makeCreateAction()
+        
+        Observable.zip(listTableView.rx.modelSelected(Memo.self), listTableView.rx.itemSelected)
+            .do(onNext: { [unowned self] (_, IndexPath) in
+                self.listTableView.deselectRow(at: IndexPath, animated: true)
+            } )
+            .map { $0.0 }
+            .bind(to: viewModel.detailAction.inputs)
+            .disposed(by: rx.disposeBag)
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
