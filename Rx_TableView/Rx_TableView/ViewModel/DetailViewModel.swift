3eca3d67749bdab16d6708d6600f2ec7fb66e501
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-10 22:09:18 +0900

    [Rx_TableView] Add ViewModelFiles

---
+++ Rx_TableView/Rx_TableView/ViewModel/DetailViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewModel.swift
+//  Rx_TableView
+//
+//  Created by DohyunKim on 2020/08/10.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class DetailViewModel {
+    
+    let foodImg = foodImgModel()
+    let view = DetailVC()
+    
+    func backToMainView() {
+        
+    }
+}
---
+++ Rx_TableView/Rx_TableView/ViewModel/MainViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewModel.swift
+//  Rx_TableView
+//
+//  Created by DohyunKim on 2020/08/10.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MainViewModel {
+    
+    let foodModel = FoodModel()
+    let view = MainVC()
+    
+    func presentView() {
+        
+        
+    }
+}
