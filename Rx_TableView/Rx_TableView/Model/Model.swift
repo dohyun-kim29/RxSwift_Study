4bda71d60f42083e37209eb486c8dda3b1decca2
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-10 22:04:25 +0900

    [Rx_TableView] Add ModelFile

--- Rx_TableView/Rx_TableView/Model/Model.swift
+++ Rx_TableView/Rx_TableView/Model/Model.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2020 DohyunKim. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FoodModel {
+    
+    let food: Array<String> = ["마라탕", "초콜릿", "마카롱", "아이스크림"]
+    
+}
+
+class foodImgModel {
+    
+    let maraImg = UIImage(named: "마라탕")
+    let chocoImg = UIImage(named: "초콜릿")
+    let makaronImg = UIImage(named: "마카롱")
+    let iceImg = UIImage(named: "아이스크림")
+    
+}
+
