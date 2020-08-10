4f5f71f842d212d27b383ac1e8e3b2a1abe027d5
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-10 22:24:00 +0900

    [Rx_TableView] Add FoodCell

---
+++ Rx_TableView/Rx_TableView/View/FoodCell.swift
@@ -0,0 +1,15 @@
+//
+//  FoodCell.swift
+//  Rx_TableView
+//
+//  Created by DohyunKim on 2020/08/10.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+
+class FoodCell: UITableViewCell {
+
+    @IBOutlet weak var foodLabel: UILabel!
+    
+}
--- Rx_TableView/Rx_TableView/View/MainVC.swift
+++ Rx_TableView/Rx_TableView/View/MainVC.swift
@@ -17,3 +17,4 @@ class MainVC: UIViewController {
         
     }
 }
+
