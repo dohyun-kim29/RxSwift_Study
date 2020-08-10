373b9a4959f972104ec941e5fc7319cfb6fb758a
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-10 21:38:10 +0900

    [Rx_TableView] Add basicUI

--- Rx_TableView/Rx_TableView/ViewController.swift
+++ Rx_TableView/Rx_TableView/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
