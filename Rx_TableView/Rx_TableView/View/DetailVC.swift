0c4555fafd9a909a39badecd29414bd9331375e7
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-10 21:50:57 +0900

    [Rx_TableView] Add ViewFiles

---
+++ Rx_TableView/Rx_TableView/View/DetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  DetailVC.swift
+//  Rx_TableView
+//
+//  Created by DohyunKim on 2020/08/10.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class DetailVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ Rx_TableView/Rx_TableView/View/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainVC.swift
+//  Rx_TableView
+//
+//  Created by DohyunKim on 2020/08/10.
+//  Copyright © 2020 DohyunKim. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MainVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
